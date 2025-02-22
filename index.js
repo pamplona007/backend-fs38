96b18b319dcc29d561665989187f7c0255352f0c
Gideão Ferreira <gideaoferreira@hotmail.com>
2025-02-22 12:00:23 -0300

    Finalização do CRUD

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import express from "express";
+import express, { request, response } from "express";
 import cors from "cors";
-import User from "./model/User.js";
 import syncTableDatabase from "./database/sync-table-database.js";
-import Order from "./model/Order.js";
 import Product from "./model/Product.js";
 
 const app = express();
@@ -11,6 +9,9 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+/**
+ * Cria um produto
+ */
 app.post("/product", async (request, response) => {
   try {
     const { name, brand, description } = request.body;
@@ -25,9 +26,53 @@ app.post("/product", async (request, response) => {
   }
 });
 
+/**
+ * Lista os produtos
+ */
 app.get("/products", async (request, response) => {
-  const products = await Product.findAll();
-  return response.status(200).json(products);
+  try {
+    const products = await Product.findAll();
+    return response.status(200).json(products);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
+});
+
+app.put("/product/:id", async (request, response) => {
+  const productId = request.params.id;
+  const productName = request.body.name;
+  const product = await Product.findOne({ where: { id: productId } });
+  product.set({
+    name: productName,
+  });
+
+  await product.save();
+  return response.status(200).json(product);
+});
+
+/**
+ * Deleta um produto
+ */
+app.delete("/product/:id", async (request, response) => {
+  try {
+    const productId = request.params.id;
+    const product = await Product.findOne({ where: { id: productId } });
+    if (!product) {
+      return response.status(422).json(`Produto não encontrado`);
+    }
+    await Product.destroy({ where: { id: productId } });
+    return response.status(200).json(`Produto deletado com sucesso`);
+  } catch (error) {
+    return response
+      .status(500)
+      .json(`Não foi possível deletar o produto: ${error.message}`);
+  }
+});
+
+app.get("/product/:id", async (request, response) => {
+  const productId = request.params.id;
+  const product = await Product.findByPk(productId);
+  return response.status(200).json(product);
 });
 
 const initApp = async () => {
